78de2b6b5e340298a86a779163626b3f5bf55355
Himanshu Kashyap <82440705+himanshukhyap@users.noreply.github.com>
2021-09-16 11:39:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 // import Datafilter from './Datafilter';
 
-import { useSelector, useDispatch } from "react-redux"
-import { Decriment, increment } from './Action/Action';
+import { useSelector, useDispatch } from "react-redux";
+import { Decriment, increment } from "./Action/Action";
 
 function App() {
   const mystate = useSelector((state) => state.changethenumbe);
   const dispatch = useDispatch();
-    return(  
-  
-    
+  return (
     <div className="container d-flex justify-content-center mt-5">
-      <a href="" className="py-2 border px-3 bg-dark text-white text-decoration-none" onClick={()=>dispatch(Decriment())}><span>-</span></a>
-      <input name="quantity" type="text" className="py-2 border px-3 bg-light text-dark text-center" value={mystate}/>
-      <a href="#" className="py-2 border px-3 bg-dark text-white text-decoration-none" onClick={()=>dispatch(increment())}><span>+</span></a>
+      <button
+        href=""
+        className="py-2 border px-3 bg-dark text-white text-decoration-none"
+        onClick={() => dispatch(Decriment())}
+      >
+        <span className="fw-bold fs-4">-</span>
+      </button>
+      <input
+        name="quantity"
+        type="text"
+        className="py-2 border px-3 bg-light text-dark text-center"
+        value={mystate}
+      />
+      <button
+        className="py-2 border px-3 bg-dark text-white text-decoration-none"
+        onClick={() => dispatch(increment())}
+      >
+        <span className="fw-bold fs-4">+</span>
+      </button>
     </div>
-    
-  )
-    
+  );
 }
 
 export default App;
