501b64eb854a2800b885de131af2bd9c691f74ce
Himanshu Kashyap <82440705+himanshukhyap@users.noreply.github.com>
2021-09-17 13:32:05 +0530

    Update UsedReducer.js

--- src/UsedReducer/UsedReducer.js
+++ src/UsedReducer/UsedReducer.js
@@ -1,6 +1,6 @@
-export default function UsedReducer(state, dispatch) {
+export default function UsedReducer(state, Action) {
 
-switch(dispatch.type){
+switch(Action.type){
     case "increment":return state+1;
     case "decrement":return state-1;
     default:return state;
